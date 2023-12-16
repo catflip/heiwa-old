cfe3ff1421848b41c7f6e5296483b675f2c3c2ab
catflip <catflip@afra.id>
2023-12-17 00:34:34 +0100

    feat: Window handler

---
+++ src/window.rb
@@ -0,0 +1,54 @@
+require_relative 'util'
+
+class Window
+  attr_reader :window_ptr, :renderer
+
+  # @type [Object] The window pointer
+  @window_ptr = nil
+
+  # @type [Object] The renderer pointer
+  @renderer = nil
+
+  def initialize(options)
+    options[:title] ||= ''
+    options[:x] ||= 0
+    options[:y] ||= 0
+    options[:width] ||= 512
+    options[:height] ||= 512
+    options[:background_color] ||= Color.new(0, 0, 0, 255)
+
+    @options = options
+
+    initialize_window
+  end
+
+  def initialize_window
+    # @type [Array<Proc>]
+    post_actions = []
+
+    # Set hints if needed
+    if @options[:type] == :dock
+      Architect.set_hint('SDL_X11_WINDOW_TYPE', '_NET_WM_WINDOW_TYPE_DOCK')
+      post_actions << -> { Architect.set_hint('SDL_HINT_X11_WINDOW_TYPE', '') }
+    end
+
+    # Spawn windows
+    @window_ptr = Architect.create_window(@options)
+
+    # Create the renderer
+    @renderer = Architect.create_renderer(@window_ptr)
+
+    # Run post actions
+    post_actions.each(&:call)
+  end
+
+  def render
+    # Clear the screen using the background color
+    Architect.render_draw_color(@renderer, *@options[:background_color].to_a)
+    Architect.render_clear @renderer
+
+    yield
+
+    Architect.render_present @renderer
+  end
+end
