2953b3f9dd9c77166c2a149a220741bd8f033512
catflip <catflip@afra.id>
2023-12-17 00:32:17 +0100

    fix(architect): allow `x` & `y` props for windows

--- ext/architect/render.c
+++ ext/architect/render.c
@@ -4,23 +4,28 @@ VALUE create_window(VALUE _self, VALUE window_params) {
 	Check_Type(window_params, T_HASH);
 
 	VALUE _title = rb_hash_aref(window_params, ID2SYM(rb_intern("title")));
+	VALUE _x = rb_hash_aref(window_params, ID2SYM(rb_intern("x")));
+	VALUE _y = rb_hash_aref(window_params, ID2SYM(rb_intern("y")));
 	VALUE _width = rb_hash_aref(window_params, ID2SYM(rb_intern("width")));
 	VALUE _height = rb_hash_aref(window_params, ID2SYM(rb_intern("height")));
 
 	Check_Type(_title, T_STRING);
+	Check_Type(_x, T_FIXNUM);
+	Check_Type(_y, T_FIXNUM);
 	Check_Type(_width, T_FIXNUM);
 	Check_Type(_height, T_FIXNUM);
 
-	char* title = StringValueCStr(_title);
+	char *title = StringValueCStr(_title);
+	int x = NUM2INT(_x);
+	int y = NUM2INT(_y);
 	int width = NUM2INT(_width);
 	int height = NUM2INT(_height);
 
 	SDL_Window *window = SDL_CreateWindow(
 		title,
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		x, y,
 		width, height,
-		0
-	);
+		0);
 
 	VALUE object = Data_Wrap_Struct(rb_cObject, 0, free_ptr, window);
 
