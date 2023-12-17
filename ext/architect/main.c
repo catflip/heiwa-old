79704610b0094a02f69c01dfacd9d741daf68d40
catflip <catflip@afra.id>
2023-12-17 20:11:08 +0100

    ref(architect): var. name `module` -> `architect`

--- ext/architect/main.c
+++ ext/architect/main.c
@@ -22,17 +22,17 @@ VALUE init_sdl()
 
 void Init_architect()
 {
-	VALUE module = rb_define_module("Architect");
+	VALUE architect = rb_define_module("Architect");
 
-	rb_define_module_function(module, "ping", info, 0);
-	rb_define_module_function(module, "init_sdl", init_sdl, 0);
-	rb_define_module_function(module, "set_hint", set_hint, 2);
+	rb_define_module_function(architect, "ping", info, 0);
+	rb_define_module_function(architect, "init_sdl", init_sdl, 0);
+	rb_define_module_function(architect, "set_hint", set_hint, 2);
 
-	rb_define_module_function(module, "create_window", create_window, 1);
-	rb_define_module_function(module, "create_renderer", create_renderer, 1);
+	rb_define_module_function(architect, "create_window", create_window, 1);
+	rb_define_module_function(architect, "create_renderer", create_renderer, 1);
 
-	rb_define_module_function(module, "render_clear", render_clear, 1);
-	rb_define_module_function(module, "render_present", render_present, 1);
-	rb_define_module_function(module, "render_draw_color", render_draw_color, 5);
-	rb_define_module_function(module, "render_rectangle", render_rectangle, 5);
+	rb_define_module_function(architect, "render_clear", render_clear, 1);
+	rb_define_module_function(architect, "render_present", render_present, 1);
+	rb_define_module_function(architect, "render_draw_color", render_draw_color, 5);
+	rb_define_module_function(architect, "render_rectangle", render_rectangle, 5);
 }
