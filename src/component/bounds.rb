2185e3a614c915ffe4805f1d5abfc2d9e25c60b9
catflip <catflip@afra.id>
2023-12-22 17:11:48 +0100

    feat: `Bounds` module
    The Bounds module helps with calculations related to a component's
    bounding box. For now, this helps the Rectangle component see if it's
    behing hovered over or not.

---
+++ src/component/bounds.rb
@@ -0,0 +1,18 @@
+# A small module for calculating position and size related things.
+module Bounds
+  def within_bounds?(position_x, position_y)
+    if  !instance_variable_defined?(:@x) ||
+        !instance_variable_defined?(:@y) ||
+        @x.nil? || @y.nil?
+      raise 'Cannot calculate bounds without `@x` and `@y`!'
+    end
+
+    if  !instance_variable_defined?(:@width) ||
+        !instance_variable_defined?(:@height) ||
+        @width.nil? || @height.nil?
+      raise 'Cannot calculate bounds without `@width` and `@height`!'
+    end
+
+    @x <= position_x && position_x <= @x + @width && @y <= position_y && position_y <= @y + @height
+  end
+end
