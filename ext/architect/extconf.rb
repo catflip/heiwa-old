a9857e7676e948b748ecfe7fd395567c17e68ee4
catflip <catflip@afra.id>
2023-12-26 22:27:07 +0100

    feat(architect): text rendering via SDL2_ttf

--- ext/architect/extconf.rb
+++ ext/architect/extconf.rb
@@ -3,7 +3,6 @@ require 'mkmf'
 have_header 'ruby.h' or missing 'ruby.h'
 
 # Compile with SDL2
-$LDFLAGS << ' -lSDL2'
-$LDFLAGS << ' -lSDL2_gfx'
+$LDFLAGS << ' -lSDL2 -lSDL2_gfx -lSDL2_ttf'
 
 create_makefile 'heiwa/architect'
--- ext/architect/render.h
+++ ext/architect/render.h
@@ -3,6 +3,7 @@
 #include <ruby.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
 
 #include "util.h"
 #include "gfx.h"
@@ -20,6 +21,18 @@ VALUE create_window(VALUE _self, VALUE window_params);
 // Creates a renderer for a window, and returns the pointer.
 VALUE create_renderer(VALUE _self, VALUE window);
 
+// Opens and returns an SDL font
+VALUE open_font(VALUE _self, VALUE font, VALUE size);
+
+// Closes an SDL font
+VALUE close_font(VALUE _self, VALUE font_obj);
+
+// Returns the size of a font
+int* font_size(TTF_Font *font, const char* text);
+
+// Renders text
+VALUE render_text(VALUE _self, VALUE renderer, VALUE x, VALUE y, VALUE content, VALUE font);
+
 // Runs `SDL_Delay`
 VALUE render_delay(VALUE _self, VALUE d);
 
@@ -34,6 +47,8 @@ VALUE render_draw_color(VALUE _self, VALUE renderer, VALUE r, VALUE g, VALUE b, 
 
 // Runs `SDL_FillRect`
 VALUE render_rectangle(VALUE _self, VALUE renderer, VALUE x, VALUE y, VALUE w, VALUE h);
+
+// Renders text
 
 // Draws a rounded rectangle.
 VALUE render_rounded_rectangle(VALUE _self, VALUE renderer, VALUE x, VALUE y, VALUE w, VALUE h, VALUE rad);
