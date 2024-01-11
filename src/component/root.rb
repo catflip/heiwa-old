6cbce5166b53ee0d3ef075fdb3e4108bd238b2f5
catflip <catflip@afra.id>
2024-01-11 20:48:27 +0100

    fix: allow dynamic size within `Root`

--- src/component/root.rb
+++ src/component/root.rb
@@ -1,7 +1,15 @@
 class Root < Component
+  include DynamicSize
   include EventHandler
 
+  reactive_accessor :width, :height
+
   def initialize
+    set(
+      { width: :max, height: :max },
+      force: true
+    )
+
     super({ x: 0, y: 0 })
   end
 
