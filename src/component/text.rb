43fa85dec183a9d58f169bd3dd62554bb225fb76
catflip <catflip@afra.id>
2024-01-14 20:56:47 +0100

    feat: readable `Text` width

--- src/component/text.rb
+++ src/component/text.rb
@@ -4,6 +4,7 @@ class Text < Component
   include EventHandler
 
   reactive_accessor :content, :font, :color, :size
+  reactive_reader :width
 
   # Creates Text.
   #
@@ -16,13 +17,20 @@ class Text < Component
   # @option options [Color] :color
   def initialize(options)
     set(
-      { content: '', font: nil, size: 16, color: Color.new(255, 255, 255) },
+      {
+        content: '', font: nil, size: 16, width: 0,
+        color: Color.new(255, 255, 255)
+      },
       force: true
     )
 
     super(options)
 
     @font_family = font.open(size)
+
+    # Update text width
+    watch(@content) { update_width }
+    update_width
   end
 
   def render(renderer)
@@ -31,5 +39,11 @@ class Text < Component
 
     Architect.render_draw_color(renderer, *color.to_a)
     Architect.render_text(renderer, pos_x, pos_y, content, @font_family)
+  end
+
+  private
+
+  def update_width
+    @width.value = Architect.text_width(@font_family, content)
   end
 end
