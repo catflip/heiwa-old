bcb37d6bb5944c8f96bdbc6917d8f04dfcdffe8d
catflip <catflip@afra.id>
2024-01-23 18:23:50 +0100

    chore(architect): method documentation
    - Added comment markers for better organization
    - Added documentation for shader and render methods
    - Fixed the order of methods to represent the order found in `main.c`.

--- lib/architect.rb
+++ lib/architect.rb
@@ -5,6 +5,10 @@ module Architect
   #   Test method. Will return "Pong!" if successful.
   #   @return [String]
 
+  ##
+  ## OpenGL methods
+  ##
+
   # @!method self.gl_init()
   #   Initializes GLFW.
   #   @return [Object]
@@ -20,6 +24,10 @@ module Architect
   #   @option options [Integer] :y
   #   @option options [Integer] :width
   #   @option options [Integer] :height
+
+  # @!method self.gl_make_context(window)
+  #   Sets the current context to the one provided.
+  #   @param [Object] window
 
   # @!method self.gl_swap_buffers(window)
   #   Swaps the GL buffer on a window.
@@ -37,9 +45,77 @@ module Architect
   #   @param [Symbol] mask The mask to use.
   #     Valid options are: `:color_buffer_bit`
 
-  # @!method self.gl_make_context(window)
-  #   Sets the current context to the one provided.
-  #   @param [Object] window
+  # @!method self.gl_bind_vertex_array(vao)
+  #   Binds a VAO.
+  #   @param [Object] vao
+
+  # @!method self.gl_draw_arrays(mode, first, count)
+  #   Draws the current VBO.
+  #   @param [Symbol] mode The draw mode.
+  #     Valid options are: `:points`, `:triangles`.
+  #   @param [Integer] first Starting index.
+  #   @param [Integer] count Number of verts.
+
+  # @!method self.gl_draw_elements(mode, count, type, indices)
+  #   Draws the current EBO.
+  #   @param [Symbol] mode The draw mode.
+  #     Valid options are: `:points`, `:triangles`.
+  #   @param [Integer] count Number of verts.
+  #   @param [Symbol] type The indices type.
+  #     Valid options are: `:unsigned_int`.
+  #   @param [Object, NilClass] indices Optional indices object.
+
+  ##
+  ## Shader methods
+  ##
+
+  # @!method self.compile_vertex_shader(source)
+  #   Compiles a vertex shader.
+  #   @param [String] source The vertex shader.
+  #   @return [Object] The compiled shader.
+
+  # @!method self.compile_fragment_shader(source)
+  #   Compiles a fragment shader.
+  #   @param [String] source The fragment shader.
+  #   @return [Object] The compiled shader.
+
+  # @!method self.delete_shader(shader)
+  #   Deletes a compiled shader.
+  #   @param [Object] shader The shader to delete.
+
+  # @!method self.create_shader_program()
+  #   Creates a shader program.
+  #   @return [Object] The shader program.
+
+  # @!method self.attach_shader_to(program, shader)
+  #   Attaches a shader to a shader program.
+  #   @param [Object] program The program to attach to.
+  #   @param [Object] shader The shader to attach.
+
+  # @!method self.link_shader_program(program)
+  #   Links a shader program.
+  #   @param [Object] program The program to link.
+
+  # @!method self.use_shader_program(program)
+  #   Uses a shader program.
+  #   @param [Object] program The program to use.
+
+  ##
+  ## Geometry methods
+  ##
+
+  # @!method self.geometry_rectangle(options)
+  # Returns the VBO and VAO for a rectangle.
+  # @param [Hash] options The rectangle options.
+  # @option options [Integer] x1
+  # @option options [Integer] y1
+  # @option options [Integer] x2
+  # @option options [Integer] y2
+  # @return [Hash] The VBO and VAO.
+
+  ##
+  ## Other
+  ##
 
   # @!method self.create_window(options)
   #   Creates a window.
